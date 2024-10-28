f187b633e0ae67804aa45eddcf1393f413dcf2bd
Ismail <ismaily@uio.no>
2024-10-28 03:58:32 +0100

    Setup with relevant parts from examples. Minior edits to evaluator.

---
+++ in5490/sequential_multi_env/config.py
@@ -0,0 +1,8 @@
+from revolve2.standards.modular_robots_v1 import gecko_v1
+
+POPULATION_SIZE = 50
+OFFSPRING_SIZE = 25
+NUM_GENERATIONS = 200
+NUM_SIMULATORS = 8
+INITIAL_STD = 0.5
+BODY = gecko_v1()
