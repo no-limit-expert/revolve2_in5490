18f0bcf1f64314abc5fd082e5768f8e860592ab9
Ismail <ismaily@uio.no>
2024-11-12 21:45:16 +0100

    Changes to config

--- in5490/sequential_multi_env/config.py
+++ in5490/sequential_multi_env/config.py
@@ -6,7 +6,7 @@ from revolve2.standards import terrains
 DATABASE_FILE = "database.sqlite"
 NUM_REPETITIONS = 1
 NUM_SIMULATORS = 1
-NUM_PARALLEL_PROCESSES = 100
+NUM_PARALLEL_PROCESSES = 50
 INITIAL_STD = 0.5
 
 NUM_BRAIN_GENERATIONS = 30
@@ -19,6 +19,6 @@ NUM_BODY_GENERATIONS = 150
 KAPPA = 3
 NU = 2.5
 LENGTH_SCALE = 0.2
-NOISE_ALPHA = 0
+NOISE_ALPHA = 1e-10
 
 ENVS = [terrains.flat(), terrains.hills(length=10, height=0.25, num_edges= 50), terrains.steps(height=0.25)]
