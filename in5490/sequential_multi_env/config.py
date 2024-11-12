02ccb06448ae2219708ff617c3de73ad3e19f613
Ismail <ismaily@uio.no>
2024-11-12 21:40:06 +0100

    Removed rugged terrain from config, causes clipping

--- in5490/sequential_multi_env/config.py
+++ in5490/sequential_multi_env/config.py
@@ -12,13 +12,13 @@ INITIAL_STD = 0.5
 NUM_BRAIN_GENERATIONS = 30
 # BODY = spider_v2()
 
-POPULATION_SIZE = 100
-OFFSPRING_SIZE = 25
-NUM_BODY_GENERATIONS = 99
+POPULATION_SIZE = 150
+OFFSPRING_SIZE = 50
+NUM_BODY_GENERATIONS = 150
 
 KAPPA = 3
 NU = 2.5
 LENGTH_SCALE = 0.2
 NOISE_ALPHA = 0
 
-ENVS = [terrains.flat(), terrains.crater((10, 10), 0.3, 0, 0.1), terrains.steps(height=0.25)]
+ENVS = [terrains.flat(), terrains.hills(length=10, height=0.25, num_edges= 50), terrains.steps(height=0.25)]
